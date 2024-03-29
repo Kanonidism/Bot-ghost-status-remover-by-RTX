71ec167e3e9fc4331d7e896c05828fa08fbf1afb
Kanonidism <162639922+Kanonidism@users.noreply.github.com>
2024-03-29 21:01:32 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening kappa","Watching Kappa"];
 
 
 let currentIndex = 0;
