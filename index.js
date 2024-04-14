098becc34775076d5e791ed5a8b6da7142e8dd07
Kanonidism <162639922+Kanonidism@users.noreply.github.com>
2024-04-14 19:51:07 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching KAPPA","Watching KAPPA"];
+const statusMessages = ["Watching Troll Kingdom","Watching Troll Kingdom"];
 
 
 let currentIndex = 0;
